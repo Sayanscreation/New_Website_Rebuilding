3df768b43192f77c34018e0ca793b2687c538427
Sayan Bhattacharjee <bhattacharjeesayan1999@gmail.com>
2025-01-16 19:55:14 +0530

    custom colour added

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        customGreen: 'rgba(55, 181, 77, 1)', // Add your custom green color here
+        customBlue: 'rgba(0, 174, 235, 1)',
+      },
+    },
   },
   plugins: [
     require('daisyui'),
