6885a667cddc25c8a92ff8352abaa31b9b8fa392
Sayan Bhattacharjee <bhattacharjeesayan1999@gmail.com>
2025-01-16 19:58:17 +0530

    Animation added

--- Frontend/src/components/Banner.jsx
+++ Frontend/src/components/Banner.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import Aos from 'aos';
+import 'aos/dist/aos.css';
 
 function Banner() {
     return (
@@ -9,20 +11,20 @@ function Banner() {
                     className="min-h-[70vh] sm:min-h-[90vh] w-full bg-cover bg-center flex items-center"
                     style={{ backgroundImage: `url('/Banner.jpg')` }}>
                     {/* Transparent Text Container */}
-                    <div className="bg-black bg-opacity-50 text-white p-4 sm:p-8 rounded-md mx-4 max-w-md lg:max-w-xl mt-20 sm:mt-20">
+                    <div className="bg-black bg-opacity-50 text-white p-4 sm:p-8 rounded-md mx-4 max-w-md lg:max-w-xl mt-20 sm:mt-20" data-aos="fade-right">
                         <h1 className="text-2xl sm:text-4xl font-bold mb-4">
                         Engineering Success Together. 
                         </h1>
-                        <p className="text-sm sm:text-base mb-4">
+                        {/* <p className="text-sm sm:text-base mb-4">
                         At Bengal Engineers, we leverage expertise and innovation to engineer solutions that propel your success forward.
                         </p>
                         <p className='mb-4'>
                         Bengal Engineers delivers client-focused civil and environmental engineering solutions to public and private clients. Our integrated approach prioritizes optimization, minimizing risk and costs for our clients. Based in Central Maryland, Bengal Engineers is a small firm that ensures local expertise, swift response times, and cost-effectiveness without compromising quality. 
-                        </p>
+                        </p> */}
 
                         <a
                             href="/about-us#"
-                            className="btn bg-blue-500 hover:bg-blue-700 text-white px-4 py-2 sm:px-6 sm:py-3">
+                            className="btn bg-gradient-to-r from-customGreen to-customBlue hover:from-blue-500 hover:to-teal-400 ... text-white px-4 py-2 sm:px-6 sm:py-3">
                             Learn More
                         </a>
                     </div>
