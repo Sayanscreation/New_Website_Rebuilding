1fc1aba8746b11baa5d35dc99b12e4d16065288e
Sayan Bhattacharjee <bhattacharjeesayan1999@gmail.com>
2025-01-28 12:21:33 +0530

    custom colours added

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -9,6 +9,7 @@ export default {
       colors: {
         customGreen: 'rgba(55, 181, 77, 1)', // Add your custom green color here
         customBlue: 'rgba(0, 174, 235, 1)',
+        customHvrBlue: 'rgba(12, 160, 214, 0.89)'
       },
     },
   },
