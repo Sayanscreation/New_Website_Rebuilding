cf4541ecdc4a3326df8636aadf8d1779f6fd69f4
Sayan Bhattacharjee <bhattacharjeesayan1999@gmail.com>
2025-02-04 12:47:11 +0530

    new background added

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -4,8 +4,12 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+
   theme: {
     extend: {
+      backgroundImage: {
+        'diagonal-thin-yellow': "repeating-linear-gradient(45deg, #FACC15 0px, #FACC15 1px, transparent 2px, transparent 4px)",
+      },
       colors: {
         customGreen: 'rgba(55, 181, 77, 1)', // Add your custom green color here
         customBlue: 'rgba(0, 174, 235, 1)',
